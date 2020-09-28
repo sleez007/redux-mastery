0ef63eb058e9f44682647ee51189844fc51b0583
Etoka Kingsley <etokakingsley@gmail.com>
2020-09-28 23:59:47 +0100

    Updates

---
+++ precache-manifest.11db91d73baf514dc36c385073782386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850271513c4a6be5ea1c87c385eb084f",
+    "url": "/redux-mastery/index.html"
+  },
+  {
+    "revision": "95fadf7b09ded811b60c",
+    "url": "/redux-mastery/static/css/main.c833627c.chunk.css"
+  },
+  {
+    "revision": "113dba0178cb33974f07",
+    "url": "/redux-mastery/static/js/2.191c4dba.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/redux-mastery/static/js/2.191c4dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fadf7b09ded811b60c",
+    "url": "/redux-mastery/static/js/main.f3de0e06.chunk.js"
+  },
+  {
+    "revision": "d4d647d3398838826d8c",
+    "url": "/redux-mastery/static/js/runtime-main.4418b591.js"
+  }
+]);
